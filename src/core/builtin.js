e43634a0a209acf5a5a8aaccf6a4a7fd3ab35bb1
yangyxu <945758912@qq.com>
2020-02-27 10:32:23 +0800

    add convertArrayArgv function for zn

--- src/core/builtin.js
+++ src/core/builtin.js
@@ -125,6 +125,57 @@
     
             return _target;
         },
+        convertArrayArgv: function (argv, options){
+            var _env = [],
+                _options = zn.extend({ prefix: '--' }, options),
+                _prefix = _options.prefix,
+                _keys = {},
+                _key = '',
+                _value = null,
+                _counter = {},
+                _started = false;
+            for(var i = 0, _len = argv.length; i < _len; i++){
+                _value = argv[i];
+                if(_value.indexOf(_prefix) !== -1){
+                    _key = _value.replace(_prefix, '');
+                    _keys[_key] = true;
+                    _counter[i+1] = _key;
+                    if(!_started) {
+                        _started = true;
+                    }
+                }else{
+                    if(_started){
+                        if(_counter[i]) {
+                            _keys[_counter[i]] = _value;
+                        }else{
+                            var _i = i - 1;
+                            while (!_counter[_i] && _i > 0) {
+                                _i = _i - 1;
+                            }
+                            if(_counter[_i]){
+                                var _keyValue = _keys[_counter[_i]];
+                                if(_keyValue != null){
+                                    if(typeof _keyValue === 'string'){
+                                        _keys[_counter[_i]] = [_keyValue, _value];
+                                    } else if(typeof _keyValue === 'object'){
+                                        _keys[_counter[_i]].push(_value);
+                                    }
+                                }else {
+                                    _keys[_counter[_i]] = _value;
+                                }
+                            }
+                        }
+                    }else {
+                        _env.push(_value);
+                    }
+                }
+            }
+    
+            return {
+                env: _env,
+                argv: _keys
+            };
+        },
         overwrite: function (target){
             var _target = target||{};
             for(var i = 1, _len = arguments.length; i < _len; i++){
