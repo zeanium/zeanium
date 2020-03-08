6f0f7e12cac8ed05799580812ac43fe9a9ef2d61
yangyxu <945758912@qq.com>
2020-03-08 21:00:17 +0800

    update module.exports

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-module.exports = require('./dist/zn.minx.js');
+if(process && process.env && process.env.NODE_ENV) {
+    if(process.env.NODE_ENV == 'development') {
+        module.exports = require('./dist/zn.js');
+    }else{
+        module.exports = require('./dist/zn.minx.js');
+    }
+}else {
+    module.exports = require('./dist/zn.minx.js');
+}
